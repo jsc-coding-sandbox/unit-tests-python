1254f3ebd799c810f4530cf5135a278b9dc3a433
John C <24364368+lvl-6@users.noreply.github.com>
2021-08-02 17:51:27 +0100

    Rewrote for pytest. Use "python -m pytest" to test.

--- test_main.py
+++ test_main.py
@@ -1,23 +1,15 @@
-import unittest
-
 import main
 
 
-class TestMain(unittest.TestCase):
+def test_add_two():
+    assert main.add_two(1,2) == 3
 
-    def test_add_two(self):
-        self.assertEqual(main.add_two(1,2), 3, "Should be 3")
+def test_sub_two():
+    assert main.sub_two(2,1) == 1
 
-    def test_sub_two(self):
-        self.assertEqual(main.sub_two(2,1), 1, "Should be 1")
+def test_div_two():
+    assert main.div_two(2,2) == 1
 
-    def test_div_two(self):
-        self.assertEqual(main.div_two(2,2), 1, "Should be 1")
-
-    def test_printeger(self):
-        self.assertEqual(main.printeger(1), "1", "Should be 1")
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_printeger():
+    assert main.printeger(1) == "1"
 
