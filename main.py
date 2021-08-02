3b1600fd0c7a7bc05d80f9ff8c33a9b9f9135520
John C <24364368+lvl-6@users.noreply.github.com>
2021-08-02 18:57:29 +0100

    Moved main.py functions into lib/math.py so that we can just generate coverage reports for everything under lib/ instead of the entire project including the tests themselves.

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-def add_two(x: int, y: int) -> int:
-    return x + y
+# Nothing
 
-def sub_two(x: int, y: int) -> int:
-    return x - y
-
-def div_two(x: int, y: int) -> int:
-    return x / y
-
-def printeger(x: int) -> str:
-    return str(x)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,15 @@
-import main
+import lib.math as math
 
 
 def test_add_two():
-    assert main.add_two(1,2) == 3
+    assert math.add_two(1,2) == 3
 
 def test_sub_two():
-    assert main.sub_two(2,1) == 1
+    assert math.sub_two(2,1) == 1
 
 def test_div_two():
-    assert main.div_two(2,2) == 1
+    assert math.div_two(2,2) == 1
 
 def test_printeger():
-    assert main.printeger(1) == "1"
+    assert math.printeger(1) == "1"
 
